7927ad03d2f9d092bdef78bf09671733fa07e3dd
Taketo Shimizu <create.taketo@gmail.com>
2025-10-19 19:32:53 +0900

    依存関係を解消

--- motemesse-api/service/app/api/langchain_routes.py
+++ motemesse-api/service/app/api/langchain_routes.py
@@ -6,8 +6,8 @@ import os
 from dotenv import load_dotenv
 
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import PydanticOutputParser
+from langchain.prompts import ChatPromptTemplate
+from langchain.output_parsers import PydanticOutputParser
 from pydantic import Field
 
 from ...modules.database import get_db
